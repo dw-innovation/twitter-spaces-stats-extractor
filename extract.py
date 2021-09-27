06ace62ff2c9bc47c730bf1a292decb1c24d646b
Andy Giefer <andreas.giefer@dw.com>
2021-09-27 12:15:26 +0200

    Added possibility to process iPhone recordings for DLF

--- extract.py
+++ extract.py
@@ -9,125 +9,277 @@ import time
 import datetime
 import statistics
 
-# maximum increase if listener number wrt to total listeners
-maximumListenerIncrease = 1 # 100%
+def extractMacCSV(samplingIntervalInSeconds, creationDatetime, outputCSVFilename):
 
-# sampling interval in seconds
-samplingIntervalInSeconds = 1
+    # maximum increase if listener number wrt to total listeners
+    maximumListenerIncrease = 1 # 100%
 
-# movie file is cmdline argument
-inputPath = sys.argv[1]
+    # movie file is cmdline argument
+    inputPath = sys.argv[1]
 
-# output file is 2. cmdline argument
-outputPath = "out.csv"
-if len(sys.argv) > 2:
-    outputPath = sys.argv[2]
+    # start output and write header
+    outputFile = open(outputCSVFilename, 'w')
+    header = "frame, seconds, minutes, time, listeners\n"
+    outputFile.write(header)
 
-# start output and write header
-outputFile = open(outputPath, 'w')
-header = "frame, seconds, minutes, time, listeners\n"
-outputFile.write(header)
+    # When was the file recorded?
+    print(f"Creation time: {creationDatetime}")
 
-# When was the file recorded?
-creationTime = os.stat(outputPath).st_birthtime
-creationDatetime = datetime.datetime.fromtimestamp(creationTime)
-print(f"Creation time: {creationDatetime}")
+    # setup capture
+    vidcap = cv2.VideoCapture(inputPath)
 
-# setup capture
-vidcap = cv2.VideoCapture(inputPath)
+    # get framerate
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    print(f"Frame rate: {fps} fps")
 
-# get framerate
-fps = vidcap.get(cv2.CAP_PROP_FPS)
-print(f"Frame rate: {fps} fps")
+    # store previous number of listeners here
+    averageNumberOfListeners = 0
 
-# store previous number of listeners here
-averageNumberOfListeners = 0
+    listenerEvolution = []
 
-listenerEvolution = []
-
-# get first frame
-success,frame = vidcap.read()
-framenbr = 0
-
-while success:
-
-    # next frame
-    framenbr += 1
-
-    # get next frame
+    # get first frame
     success,frame = vidcap.read()
-        
-    # break if we are at the end
-    if not success:
-        break
+    framenbr = 0
 
-    # update every second
-    if framenbr % int(samplingIntervalInSeconds*fps) == 0:
-        
-        # change to grey scale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while success:
 
-        # resize
-        gray = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_LINEAR)
+        # next frame
+        framenbr += 1
 
-        #cv2.imshow("Resized image", gray)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
+        # get next frame
+        success,frame = vidcap.read()
+            
+        # break if we are at the end
+        if not success:
+            break
 
-        # text extraction
-        data = pytesseract.image_to_string(gray, lang='eng', config='--psm 3').lower()
+        # update every second
+        if framenbr % int(samplingIntervalInSeconds*fps) == 0:
+            
+            # change to grey scale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # relevant part
-        extractedLines = re.findall(r'(\d+) others', data)
+            # resize
+            gray = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_LINEAR)
 
-        if len(extractedLines) == 0:
-            print(f"No extracted lines. Skipping.")
             #cv2.imshow("Resized image", gray)
             #cv2.waitKey(0)
             #cv2.destroyAllWindows()
-            continue
 
-        extracted = extractedLines[0]
+            # text extraction
+            data = pytesseract.image_to_string(gray, lang='eng', config='--psm 3').lower()
 
-        # use last 3 charcters, first one it is interpreted as '4'
-        numberOfListeners = int(extracted[-3:])
+            # relevant part
+            extractedLines = re.findall(r'(\d+k?) others', data)
 
-        # store in evolution of the listenerEvolution
-        listenerEvolution.append(numberOfListeners)
+            # skip if no result could be found
+            if len(extractedLines) == 0:
+                print(f"No extracted lines. Skipping.")
+                #cv2.imshow("Resized image", gray)
+                #cv2.waitKey(0)
+                #cv2.destroyAllWindows()
+                continue
 
-        # keep last 20 values
-        if len(listenerEvolution) > 20:
-            listenerEvolution = listenerEvolution[-20:]
+            # get relevant part
+            extracted = extractedLines[0]
 
-        
-        # median
-        median = statistics.median(listenerEvolution)
-
-        # is the increase too large - > ignore, as this is an OCR problem
-        if  abs(numberOfListeners - median) > median * maximumListenerIncrease:
-            print(f"change in number too large. Skipping")
-            print(f"extracted: {extracted}")
-            print(f"numberOfListeners: {numberOfListeners}")
-            print(f"median: {median}")
-            continue
-
-        # relative time
-        relTimeSec = framenbr/fps
-        relTimeMin = relTimeSec/60
-
-        # absolute time
-        frameDatetime = creationDatetime + datetime.timedelta(seconds=relTimeSec)
-        frameTimeString = frameDatetime.strftime("%H:%M:%S ")
-
-        # prints als CSV line
-        csvLine = f"{framenbr}, {relTimeSec}, {relTimeMin}, {frameTimeString}, {numberOfListeners}"
-        print(csvLine)
-        #print(data)
-
-        outputFile.write(csvLine + '\n')
-        outputFile.flush()
+            # determine factor - it'S 1000 if there is a 'k' at the end
+            factor = 1
+            if extracted[-1] == 'k':
+                factor = 1000
+                extracted = extracted[:-1] # loose last 'k'
 
 
+            # use last 3 charcters, first one it is interpreted as '4'
+            numberOfListeners = int(extracted[-3:]) * factor
 
+            # store in evolution of the listenerEvolution
+            listenerEvolution.append(numberOfListeners)
+
+            # keep last 20 values
+            if len(listenerEvolution) > 20:
+                listenerEvolution = listenerEvolution[-20:]
+
+            
+            # median
+            median = statistics.median(listenerEvolution)
+
+            # is the increase too large - > ignore, as this is an OCR problem
+            if  abs(numberOfListeners - median) > median * maximumListenerIncrease:
+                print(f"change in number too large. Skipping")
+                print(f"extracted: {extracted}")
+                print(f"numberOfListeners: {numberOfListeners}")
+                print(f"median: {median}")
+                continue
+
+            # relative time
+            relTimeSec = framenbr/fps
+            relTimeMin = relTimeSec/60
+
+            # absolute time
+            frameDatetime = creationDatetime + datetime.timedelta(seconds=relTimeSec)
+            frameTimeString = frameDatetime.strftime("%H:%M:%S ")
+
+            # prints als CSV line
+            csvLine = f"{framenbr}, {relTimeSec}, {relTimeMin}, {frameTimeString}, {numberOfListeners}"
+            print(csvLine)
+            #print(data)
+
+            outputFile.write(csvLine + '\n')
+            outputFile.flush()
+
+
+def extractiPhoneCSV(samplingIntervalInSeconds, creationTime, outputCSVFilename):
+
+    # maximum increase if listener number wrt to total listeners
+    maximumListenerIncrease = 1 # 100%
+
+
+
+    # movie file is cmdline argument
+    inputPath = sys.argv[1]
+
+    # start output and write header
+    outputFile = open(outputCSVFilename, 'w')
+    header = "frame, seconds, minutes, time, listeners\n"
+    outputFile.write(header)
+
+    # When was the file recorded?
+    print(f"Creation time: {creationDatetime}")
+
+    # setup capture
+    vidcap = cv2.VideoCapture(inputPath)
+
+    # get framerate
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    print(f"Frame rate: {fps} fps")
+
+    # store previous number of listeners here
+    averageNumberOfListeners = 0
+
+    listenerEvolution = []
+
+    # get first frame
+    success,frame = vidcap.read()
+    framenbr = 0
+
+    while success:
+
+        # next frame
+        framenbr += 1
+
+        # get next frame
+        success,frame = vidcap.read()
+            
+        # break if we are at the end
+        if not success:
+            break
+
+        # update every second
+        if framenbr % int(samplingIntervalInSeconds*fps) == 0:
+            
+            # change to grey scale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # resize
+            gray = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_LINEAR)
+
+            #cv2.imshow("Resized image", gray)
+            #cv2.waitKey(0)
+            #cv2.destroyAllWindows()
+
+            # text extraction
+            data = pytesseract.image_to_string(gray, lang='eng', config='--psm 3').lower()
+
+            # default exaction
+            extracted = None
+
+            # split OCR result into lines
+            dataLines = data.split('\n')
+
+            # go though each extracted line
+            for dataLine in dataLines:
+
+                #print(dataLine)
+
+                # search for relevant parts
+                extractedGroups = re.findall(r'(\d+k?) (?:others|weitere)', dataLine)
+
+                #print(extractedGroups)
+                #print("************")
+
+                # if we found something
+                if len(extractedGroups) > 0:
+                    extracted = extractedGroups[0]
+                    break
+                else:
+                    continue
+
+            # skip if no data could be found
+            if extracted is None:
+                    # skip if no result could be found
+                    print(f"No extracted data. Skipping.")
+                    continue
+
+            # determine factor - it'S 1000 if there is a 'k' at the end
+            factor = 1
+            if extracted[-1] == 'k':
+                factor = 1000
+                extracted = extracted[:-1] # loose last 'k'
+
+
+            # use last 3 charcters, first one it is interpreted as '4'
+            #numberOfListeners = int(extracted[-3:]) * factor
+            numberOfListeners = int(extracted) * factor
+
+            # store in evolution of the listenerEvolution
+            listenerEvolution.append(numberOfListeners)
+
+            # keep last 20 values
+            if len(listenerEvolution) > 20:
+                listenerEvolution = listenerEvolution[-20:]
+
+            
+            # median
+            median = statistics.median(listenerEvolution)
+
+            # is the increase too large - > ignore, as this is an OCR problem
+            if  abs(numberOfListeners - median) > median * maximumListenerIncrease:
+                print(f"change in number too large. Skipping")
+                print(f"extracted: {extracted}")
+                print(f"numberOfListeners: {numberOfListeners}")
+                print(f"median: {median}")
+                continue
+
+            # relative time
+            relTimeSec = framenbr/fps
+            relTimeMin = relTimeSec/60
+
+            # absolute time
+            frameDatetime = creationDatetime + datetime.timedelta(seconds=relTimeSec)
+            frameTimeString = frameDatetime.strftime("%H:%M:%S ")
+
+            # prints als CSV line
+            csvLine = f"{framenbr}, {relTimeSec}, {relTimeMin}, {frameTimeString}, {numberOfListeners}"
+            print(csvLine)
+            #print(data)
+
+            outputFile.write(csvLine + '\n')
+            outputFile.flush()
+
+if __name__ == '__main__':
+
+    # sampling interval in seconds
+    samplingIntervalInSeconds = 5
+
+    # When was the file recorded?
+    #creationTime = os.stat(outputPath).st_birthtime
+    #creationDatetime = datetime.datetime.fromtimestamp(creationTime)
+    #creationDatetime = datetime.datetime(year=2021, month=9, day=26, hour=21, minute=56, second=00)
+    #extractMacCSV(samplingIntervalInSeconds, creationDatetime, 'mac.csv')
+    
+    
+    creationDatetime = datetime.datetime(year=2021, month=9, day=26, hour=22, minute=47, second=18)
+    extractiPhoneCSV(samplingIntervalInSeconds, creationDatetime, 'iPhone.csv')
 
   
--- plot.gnu
+++ plot.gnu
@@ -1,15 +1,25 @@
 set term png font "Palatino, 14" size 1280, 720
-set output 'graph.png'
+set output '@deutschlandfunk Twitter Space 2021-09-26 22 CEST.png'
 
 set key autotitle columnhead
 set boxwidth 0.5 relative
 set datafile separator ","
-set xlabel 'minutes'
+
 set ylabel '# of listeners'
 set grid
+
+#set xlabel 'minutes'
+set xlabel 'Time (CEST)'
+
+set xdata time
+set timefmt "%H:%M:%S"
+set format x "%H:%M"
+
+set yrange [0:1100]
+
 #set style data histogram
 #set style fill solid
 
-set title 'Evolution of listeners: Fact or Cap 2021-09-20 11 CEST'
+set title 'Evolution of listeners: Deutschlandfunk Twitter Space 2021-09-26 22 CEST'
 
-plot 'out.csv' u 3:5 w lines
+plot 'mac.csv' u 4:5 w lines

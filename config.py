7af8c9f8947448487e0988dcac7f97e7f6c72a52
Andy Giefer <andreas.giefer@dw.com>
2022-03-15 17:35:53 +0100

    updated for Twitter Space 2022-03-22

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import datetime
 graphFolder = "graphs"
 
 # at what time did the recording of the video file start?
-videoCreationDatetime = datetime.datetime(year=2021, month=11, day=4, hour=14, minute=1, second=42)
+videoCreationDatetime = datetime.datetime(year=2022, month=3, day=15, hour=12, minute=59, second=44)
 
 # evaluate a video frame every <samplingIntervalInSeconds>
 samplingIntervalInSeconds = 1
@@ -13,13 +13,13 @@ samplingIntervalInSeconds = 1
 graphTitle = "Evolution of Twitter Space listeners"
 
 # where does the time range start on the graphs?
-graphStartTime = "14:00:00"
+graphStartTime = "13:00:00"
 
 # where does the time range stop on the graphs?
-graphEndTime = "14:40:00"
+graphEndTime = "14:00:00"
 
 # what is the minimum number of listeners displayed in the graphs?
 graphMinimumNumberOfListeners = 0
 
 # what is the maximum number of listeners displayed in the graphs?
-graphMaximumNumberOfListeners = 1000
+graphMaximumNumberOfListeners = 600
---
+++ fillGraphGaps.py
@@ -0,0 +1,39 @@
+import glob
+import config
+import os
+import shutil
+
+def fill():
+
+    fileList = glob.glob(f"{config.graphFolder}/*.png")
+    fileList.sort()
+
+    expectedIndex = 0
+
+    for filename in fileList:
+        index = int(filename[-8:-4])
+        dirname =  os.path.dirname(filename)
+        basename = os.path.basename(filename)
+
+        print(filename, index, dirname, basename)
+
+        if index == expectedIndex:
+            lastValidFilename = filename
+
+        else:
+
+            # do we miss a file?
+            while index != expectedIndex:
+
+                newBasename = "plot%04d" % expectedIndex
+                newFilename = os.path.join(dirname, newBasename + '.png')
+
+                print(f"Copying {lastValidFilename} -> {newFilename}")
+                shutil.copyfile(lastValidFilename, newFilename)
+                expectedIndex += 1  
+
+        expectedIndex += 1        
+
+
+if __name__ == '__main__':
+    fill()
